1354dad37978f44a2da0b8d595c9712253bbd75f
serg0012 <seregawz@gmail.com>
2019-07-31 22:31:35 +0300

    create_users edited

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get '/index' => "welcome#index"
   get '/' => "welcome#index"
   root 'welcome#index'
-
-  #get "/users" => "users#index"
-  #post "/users" => "users#create"
-  #get "/users/new" => "users#new"
   resources :users
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
