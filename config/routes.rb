c33e816d69035cbec5d6caa547eb22624a9ef992
serg0012 <seregawz@gmail.com>
2019-07-31 22:15:53 +0300

    create_users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
+  get '/index' => "welcome#index"
+  get '/' => "welcome#index"
   root 'welcome#index'
+
+  #get "/users" => "users#index"
+  #post "/users" => "users#create"
+  #get "/users/new" => "users#new"
+  resources :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'vas@vas.net', first_name: 'vasya', last_name: 'vas')
+User.create(email: 'pet@pet.new', first_name: 'petya', last_name: 'pet')
+User.create(email: 'sas@sas.net', first_name: 'sasha', last_name: 'sas')
