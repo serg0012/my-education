f04873fe1a5c00f1282cd960b5ef517a590a0923
serg0012 <46287708+serg0012@users.noreply.github.com>
2019-07-31 22:37:51 +0300

    create_users (#3)
    * create_users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class UsersController < ActionController::Base
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'vas@vas.net', first_name: 'vasya', last_name: 'vas')
+User.create(email: 'pet@pet.new', first_name: 'petya', last_name: 'pet')
+User.create(email: 'sas@sas.net', first_name: 'sasha', last_name: 'sas')
